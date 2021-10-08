c3f8fee2253b55439dcfeed5de868b93bd0bd9dc
Nichlas Løkken <Loekken.n@gmail.com>
2021-10-08 14:52:42 +0200

    sidste ændringer fra video

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
+
 const btn = document.getElementById("btn");
 const sidebar = document.querySelector(".sidebar")
 const searchBtn = document.querySelector(".bx-search");
 
 btn.onclick = function(){
     sidebar.classList.toggle("active");
+    menuBtnChange();
 }
 searchBtn.onclick = function() {
     sidebar.classList.toggle("active");
+    menuBtnChange();
 }
 
-console.log(btn);
+function menuBtnChange() {
+    if(sidebar.classList.contains("active")){
+      btn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the icons class
+    }else {
+      btn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the icons class
+    }
+}
